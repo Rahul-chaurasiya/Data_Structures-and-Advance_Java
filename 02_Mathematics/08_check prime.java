4109f4701015b269f0aeafe248b10151303c76dc
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2022-12-11 11:51:43 +0530

    Update 08_check prime.java

--- 02_Mathematics/08_check prime.java
+++ 02_Mathematics/08_check prime.java
@@ -24,14 +24,15 @@ class demo {
 
 
 ==========================================================================
+	
 // efficient solution
     
 class demo {
 
     static boolean isPrime(int x){
         if(x==1) return false;
-        if(x==2) return true;
-        if(x%2==0 || x%3 == 0) return true;
+        if(x==2 || x==3) return true;
+        if(x%2==0 || x%3 == 0) return false;
         
         for(int i=5; i*i<=x ; i=i+6){                                   //for this line if we take input as 23 so we skip 1,2,3,4,6,8,9,10,12,14,15,16,18,20,21 
             if(x%i == 0 || x%(i+2) == 0) return false;                  // ab hme sirf i=5,5+2=7,
