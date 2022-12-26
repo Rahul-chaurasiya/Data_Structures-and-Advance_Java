e743b1b08dada2e68a9dc3e87b8a9b8bdbbc4de3
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2022-12-26 10:19:16 +0530

    Update 13_iterative_power.java

--- 02_Mathematics/13_iterative_power.java
+++ 02_Mathematics/13_iterative_power.java
@@ -3,10 +3,10 @@ public class j13_iterative_power {
     static int power(int x, int n) {
         int res = 1;
         while (n > 0) {
-            if (n % 2 != 0)
+            if (n % 2 != 0)             // n & 1
                 res = res * x;                       
             x = x * x;
-            n = n / 2;
+            n = n / 2;                  // n = n >> 1
         }
         return res;
     }
