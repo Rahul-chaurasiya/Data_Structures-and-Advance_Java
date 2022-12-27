61bf900aafec228d4a9189310e7624db2c0c7212
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2022-12-27 08:29:02 +0530

    Create 03_bitwize_operator.java

---
+++ 03_Bit_Magic/03_bitwize_operator.java
@@ -0,0 +1,11 @@
+
+public class j03_bitwize_operator {
+    public static void main(String[] args) {
+
+        int x = 3, y = 6;
+
+        System.out.printf("Bitwise OR ", x & y);
+        System.out.printf("Bitwise AND ", x | y);
+        System.out.printf("Bitwise XOR ", x ^ y);
+    }
+}
