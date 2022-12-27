de13394024383db195c0c014a0713149c22ce4ac
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2022-12-27 09:13:40 +0530

    Create 05_bitwize_operator.java

---
+++ 03_Bit_Magic/05_bitwize_operator.java
@@ -0,0 +1,19 @@
+
+public class j05_bitwize_operator {
+    public static void main (String[] args) {
+
+	    // Right shift operator
+	    int x = 33;
+	    System.out.println(x >> 1);
+        System.out.println(x >> 2);
+
+        // with negetive number 
+        int y = -2;
+	    System.out.println(y>> 1);
+
+        //Unsigned Right Shift
+        int z = -2;
+	    System.out.println(z >>> 1);
+	    
+	}
+}
