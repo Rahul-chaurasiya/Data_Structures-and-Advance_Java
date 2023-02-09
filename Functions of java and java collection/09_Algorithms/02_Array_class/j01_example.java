3e6c172513cc1a95eb069ee99b27eb031990595c
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-02-09 22:34:16 +0530

    Create j01_example.java

---
+++ Functions of java and java collection/09_Algorithms/02_Array_class/j01_example.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class j01_example {
+
+    public static void main(String[] args) {
+        int a[] = { 10, 20, 30 }; 
+
+        // Get the second Arrays 
+        int b[] = { 10, 20, 30 }; 
+
+        // To compare both arrays 
+        System.out.println(Arrays.equals(a, b));
+        
+        // To represent integers as a string
+        System.out.println(Arrays.toString(a));
+    }
+}
