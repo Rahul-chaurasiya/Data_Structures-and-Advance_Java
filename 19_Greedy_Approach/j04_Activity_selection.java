4df44480bfa5186528aba989c5a3a090a82dde6a
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-04-05 23:43:29 +0530

    Create j04_Activity_selection.java

---
+++ 19_Greedy_Approach/j04_Activity_selection.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Activity {
+    int start;
+    int finish;
+
+    Activity(int s, int f) {
+        start = s;
+        finish = f;
+    }
+}
+
+class MyCmp implements Comparator<Activity> {
+    public int compare(Activity a1, Activity a2) {
+        return a1.finish - a2.finish;
+    }
+}
+
+public class j04_Activity_selection {
+
+    static int maxActivity(Activity arr[], int n) {
+        Arrays.sort(arr, new MyCmp());
+
+        int res = 1;
+        int prev = 0;
+
+        for (int curr = 1; curr < n; curr++) {
+            if (arr[curr].start >= arr[prev].finish) {
+                res++;
+                prev = curr;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String args[]) {
+        Activity arr[] = { new Activity(12, 25),
+                new Activity(10, 20),
+                new Activity(20, 30) };
+        int n = 3;
+        System.out.println(maxActivity(arr, n));
+
+    }
+}
+
+
+
