4ec56a2584d8b18c035b050b779216d45aff6aeb
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-05-21 22:54:19 +0530

    Create j06_requestdispacher_success.java

---
+++ Servlet/j06_requestdispacher/j06_requestdispacher_success.java
@@ -0,0 +1,18 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class j06_requestdispacher_success extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Registration has been done succesfully</h1>");
+        out.println("This is success servlet");
+    }
+}
