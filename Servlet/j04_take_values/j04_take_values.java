3d0f7f45132a5660d3f1a08be795c739977c1b52
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-05-21 12:43:33 +0530

    Create j04_take_values.java

---
+++ Servlet/j04_take_values/j04_take_values.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class j04_take_values extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        
+        String nm = req.getParameter("username"); 
+        String mail = req.getParameter("email");
+        String pwd = req.getParameter("password");
+        
+        out.println("<b>Name = " + nm + "</b><br>");
+        out.println("<b>Password = " + pwd + "</b><br>");
+        out.print("<b>Email = " + mail + "</b>");
+    }
+}
