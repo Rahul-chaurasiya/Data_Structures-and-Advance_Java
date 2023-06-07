b624771f896aaa321391c2cd5b90b238bcfa835b
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-06-08 01:32:47 +0530

    Create j14_doubly_linkedlist.java

---
+++ 11_LinkedList/02_doubly_LinkedList/j14_doubly_linkedlist.java
@@ -0,0 +1,37 @@
+
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        prev = null;
+        next = null;
+    }
+}
+
+class j14_doubly_linkedlist {
+
+    public static void traversal(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Node head = new Node(10);
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+        head.next = temp1;
+        temp1.prev = head;
+        temp1.next = temp2;
+        temp2.prev = temp1;
+        System.out.print("our current Linkedlist is : ");
+
+    }
+
+}
