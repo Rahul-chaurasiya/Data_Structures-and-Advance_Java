7a01bd6825e3169afba1e175db8173f84408105b
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-07-02 23:46:17 +0530

    Create j51_zigzag_linkedlist.java

---
+++ 11_LinkedList/j51_zigzag_linkedlist.java
@@ -0,0 +1,85 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode right = reverse(slow.next);
+        slow.next = null;
+        ListNode left = head;
+
+        ListNode nextl;
+        ListNode nextr;
+
+        while (left != null && right != null) {
+            nextl = left.next;
+            left.next = right;
+
+            nextr = right.next;
+            right.next = nextl;
+
+            left = nextl;
+            right = nextr;
+        }
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
+
+public class j51_zigzag_linkedlist {
+    public static void main(String[] args) {
+        // Create a sample linked list
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        Solution solution = new Solution();
+        solution.reorderList(head);
+
+        // Print the modified linked list
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+}
