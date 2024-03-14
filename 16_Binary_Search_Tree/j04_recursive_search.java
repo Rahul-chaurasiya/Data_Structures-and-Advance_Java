a097b84cc1be337bb6b62b14d94aaecce03efaca
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2024-03-15 03:03:49 +0530

    Create j04_recursive_search.java

---
+++ 16_Binary_Search_Tree/j04_recursive_search.java
@@ -0,0 +1,34 @@
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    Node(int k) {
+        key = k;
+        left = right = null;
+    }
+}
+
+public class j04_recursive_search {
+
+    public static void main(String args[]) {
+        Node root = new Node(15);
+        root.left = new Node(5);
+        root.left.left = new Node(3);
+        root.right = new Node(20);
+        root.right.left = new Node(18);
+        root.right.left.left = new Node(16);
+        root.right.right = new Node(80);
+        int x = 16;
+
+        if (search(root, x)) System.out.print("Found");
+        else System.out.print("Not Found");
+    }
+
+    private static boolean search(Node root, int x) {
+        if (root == null) return false;
+        if (root.key == x) return true;
+        if (root.key > x) return search(root.left, x);
+        else return search(root.right, x);
+    }
+}
